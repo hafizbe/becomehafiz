74108a380b97bf8099e0571584aecd47378260bb
= <hafizbe@gmail.com>
2013-03-30 20:31:48 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hafiz::Application.config.secret_token = '838d7105fe946674e65e03669208a3191112d3f12ff612b18246e69b27d1d054daf5d170c124aca08d5ea74e9f80aaa65ae7d3efd6478978534531b7c6a6f59a'
