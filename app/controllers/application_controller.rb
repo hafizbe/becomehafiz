bccf44bb444617c682784ac0938f5dd762e4e9da
BecomeAHafiz <bechomeahafiz@gmail.com>
2013-07-30 03:10:10 +0200

    Access-Control-Allow-Origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  #test
+  after_filter :set_access_control_headers
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
